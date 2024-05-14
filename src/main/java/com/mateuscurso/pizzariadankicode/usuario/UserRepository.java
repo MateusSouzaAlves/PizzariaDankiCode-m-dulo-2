80b702141833169b8715c131201326a51759c59e
MateusSouzaAlves <javaamoavida@gmail.com>
2024-05-14 13:35:45 -0300

    Continunando a implementação do usuário

---
+++ src/main/java/com/mateuscurso/pizzariadankicode/usuario/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mateuscurso.pizzariadankicode.usuario;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<Usuario,Long> {
+
+    UserDetails findByUserName(String username);
+}
--- src/main/java/com/mateuscurso/pizzariadankicode/usuario/Usuario.java
+++ src/main/java/com/mateuscurso/pizzariadankicode/usuario/Usuario.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "Usuario")
+@Table(name = "usuarios")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
